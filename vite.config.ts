0de54adaf4616c7b57bf495ef1bb476cd4f3128c
Alexandre Combemorel <acombemorel@scaleway.com>
2025-01-18 18:55:27 +0100

    feat: test

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc"
 
 // https://vite.dev/config/
 export default defineConfig({
+  // @ts-expect-error wrong type provided by @vitejs/plugin-react-swc
   plugins: [react()],
   server: {
     port: 3000,
