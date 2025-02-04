08f5ffbce535d38cea05135af745e3966f454fcb
Alexandre Combemorel <acombemorel@scaleway.com>
2025-02-04 14:52:43 +0100

    fix

--- frontend/src/providers/AuthProvider/AuthProvider.tsx
+++ frontend/src/providers/AuthProvider/AuthProvider.tsx
@@ -1,33 +1,35 @@
-import type { ReactNode } from "react"
-import { createContext, useContext, useState } from "react"
-import { useEffect } from "react"
-import type { AuthContextType, UserType } from "./types"
-import { api } from "../../sdk"
+import type { ReactNode } from "react";
+import { createContext, useContext, useState } from "react";
+import { useEffect } from "react";
+import type { AuthContextType, UserType } from "./types";
+import { api } from "../../sdk";
 
 export const AuthContext = createContext<AuthContextType>({
-  user: undefined,
-  setUser: () => {},
-})
+	user: undefined,
+	setUser: () => {},
+});
 
-export const useAuth = () => useContext(AuthContext)
+export const useAuth = () => useContext(AuthContext);
 
 const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [userData, setUserData] = useState<UserType>()
+	const [userData, setUserData] = useState<UserType>();
 
-  useEffect(() => {
-    api
-      .getUser()
-      .then((user) => {
-        setUserData(user as UserType)
-      })
-      .catch(() => null)
-  }, [])
+	useEffect(() => {
+		if (api.doesUserExist()) {
+			api
+				.getUser()
+				.then((user) => {
+					setUserData(user as UserType);
+				})
+				.catch(() => null);
+		}
+	}, []);
 
-  return (
-    <AuthContext.Provider value={{ user: userData, setUser: setUserData }}>
-      {children}
-    </AuthContext.Provider>
-  )
-}
+	return (
+		<AuthContext.Provider value={{ user: userData, setUser: setUserData }}>
+			{children}
+		</AuthContext.Provider>
+	);
+};
 
-export default AuthProvider
+export default AuthProvider;
